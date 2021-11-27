3f1c7af4b17d3a4d1a4f45834197e88673568b3e
Rahul Kaplesh <rahul.kaplesh@gmail.com>
2021-11-27 15:14:07 +0530

    client update in rust
    Signed-off-by: Rahul Kaplesh <rahul.kaplesh@gmail.com>

--- src/client.rs
+++ src/client.rs
@@ -3,10 +3,9 @@ use super::common::{Message};
 use message_io::network::{NetEvent, Transport, Endpoint};
 use message_io::node::{self, NodeHandler, NodeListener};
 
-use std::net::{SocketAddr};
-use std::collections::{HashMap};
 use std::io::{self};
 
+#[derive(Clone)]
 pub struct Client {
     handler: NodeHandler<()>,
     name: String,
@@ -14,14 +13,23 @@ pub struct Client {
 }
 
 impl Client {
-    fn new(name: &str) -> io::Result<Client> {
+    pub fn new(name: &str) -> io::Result<Client> {
         let (handler, node_listener) = node::split();
         let server_endpoint = "127.0.0.1:5000"; // Connection to the discovery server.
-        let (endpoint, _) = handler.network().connect(Transport::FramedTcp, server_endpoint)?;
+        let (endpoint, address) = handler.network().connect(Transport::FramedTcp, server_endpoint)?;
+        let message = Message::RegisterClient(name.to_string().clone(), address);
+        let output_data = bincode::serialize(&message).unwrap();
+        handler.network().send(endpoint, &output_data);
         Ok(Client {
             handler,
-            name,
-            endpoint
+            name: name.to_string(),
+            server_endpoint: endpoint,
         })
+    }
+
+    pub fn send_data(self, message: &str) {
+        let message = Message::DataTransferMessage(self.name.clone(), message.to_string());
+        let output_data = bincode::serialize(&message).unwrap();
+        self.handler.network().send(self.server_endpoint, &output_data);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod common;
+mod client;
+use chrono;
+
 fn main() {
     println!("Hello, world!");
+    let temp_client = client::Client::new("Sample-1");
+    match temp_client {
+        Ok(temp) => {
+            loop {
+                let temp = Some(temp.clone());
+                let dt: chrono::DateTime<chrono::Local> = chrono::offset::Local::now();
+                temp.unwrap().send_data(dt.to_string().as_str());
+            }
+        },
+        Err(err) => println!("Cannot Instantiate Client: {}", err),
+    }    
 }
